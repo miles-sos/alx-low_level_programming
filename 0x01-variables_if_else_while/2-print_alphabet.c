7fd62aa4b05bfbd9d7a0772a5a96a1a59b020383
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 12:29:30 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,13 +3,13 @@
  * Main - prints all alphabets on a new line
  *
  * Return: 0 (success)
- *
  */
-int main(void){
+int main(void)
+{
 int i;
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 
-for (i =0; i < alphabet[i] !='\0'; i++)
+for (i = 0; i < alphabet[i] != '\0'; i++)
 {
 putchar(alphabet[i]);
 putchat('\n');
