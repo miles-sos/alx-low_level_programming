363d57460be89808caa8ae82882ac2b8ff1edeb4
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 14:42:14 -0700

    9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Print all combo of single digit nums
+*
+* Return: 0 (Success)
+*/
+int main(void)
+{
+
+int num;
+for (num = 0; num < 10; num++)
+{
+putchar('0' + num);
+if (num < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
