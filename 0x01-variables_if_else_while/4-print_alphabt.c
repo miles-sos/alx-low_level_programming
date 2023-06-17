04257f50cf586f4b7d913a5206a56f6efde8b8e6
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 13:16:40 -0700

    4-print_alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print all letters except e and q
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+if (alpha == 'q' || alpha == 'e')
+{
+continue;
+}
+else
+{
+putchar(alpha);
+}
+}
+return (0);
+}
