fad0298de229b031f57c5e3719d06bab030c185f
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 11:57:04 -0700

    1-last_digit N

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - prints all alphabets on a new line
+ *
+ * Return: 0 (success)
+ *
+ */
+int main(void){
+int i;
+char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+
+for (i =0; i < alphabet[i] !='\0'; i++)
+{
+putchar(alphabet[i]);
+putchat('\n');
+}
+return (0);
+}
