c4439ad0ca1bb86fa6a72e3d823263064cc387a7
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 11:07:24 -0700

    Final fix for The last digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 }
 else if (lastDigit == 0)
 {
-printf("Last digit of %d is $%d and is 0\n", n, lastDigit);
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
 else if (lastDigit < 6 && lastDigit != 0)
 {
