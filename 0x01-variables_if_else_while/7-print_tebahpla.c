efc24acf37452ab4f6bca624f35944d745e29103
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-17 14:12:28 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet in reverse
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+
+char alpha;
+
+for (alpha = 'z'; alpha >= 'a'; alpha--)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
