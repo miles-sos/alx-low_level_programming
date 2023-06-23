f092da5b3b5cf96103e95df46aeb34b1c29d1797
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-22 18:47:37 -0700

    Updated 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,23 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("%s ", "FizzBuzz");
+printf("FizzBuzz");
 }
 else if (i % 5 == 0)
 {
-printf("%s ", "Buzz");
+printf("Buzz");
 }
 else if (i % 3 == 0)
 {
-printf("%s ", "Fizz");
+printf("Fizz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (i != 100)
+{
+	printf(" ");
 }
 }
 printf("\n");
