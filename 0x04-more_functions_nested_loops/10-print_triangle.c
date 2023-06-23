edc73efeacb9f36ea53bafc6dfe612e4a9fccab3
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-22 19:10:26 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,23 +6,28 @@
  */
 void print_triangle(int size)
 {
-	int i = 1;
-	int count;
+	int c, i, j;
+
+	c = 0;
+	i = size - 1;
+	while (c < size)
+	{
+		i = size - 1 - c;
+		j = c + 1;
+		while (i > 0)
+		{
+			_putchar(' ');
+			i--;
+		}
+		while (j > 0)
+		{
+			_putchar('#');
+			j--;
+		}
+		_putchar('\n');
+		c++;
+	}
 
 	if (size <= 0)
-	{
 		_putchar('\n');
-	}
-	else
-	{
-		while (i <= size)
-		{
-			for (count = 1; count <= i; count++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-			i++;
-		}
-	}
 }
