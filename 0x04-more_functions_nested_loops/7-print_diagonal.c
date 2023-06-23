a3b79b23f65f86b9f839c281010f01403eecb00a
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-22 19:35:35 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,25 +8,23 @@
 
 void print_diagonal(int n)
 {
-	int i;
-	int count;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		while (i <= n)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (count = 0; count < i; count++)
+			for (j = 0; j < n; j++)
 			{
-				_putchar(' ');
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
 			}
-			_putchar('\\');
 			_putchar('\n');
-			i++;
 		}
-		_putchar('\n');
 	}
 }
