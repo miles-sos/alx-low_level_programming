02f42032a702dbea624fe6856059ece66adfadbf
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-26 13:10:55 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev -  prints a string, in reverse,
+ * followed by a new line.
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+int len;
+int x;
+len = strlen(s);
+x = len;
+
+while (x >= 0)
+{
+_putchar(s[x]);
+x--;
+}
+_putchar('\n');
+}
