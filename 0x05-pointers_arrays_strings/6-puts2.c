bbd441201792508aa54a7406fad40f869eb94359
miles-sos <frank.ebuzoeme@gmail.com>
2023-06-26 19:54:53 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+char *newStr = str;
+
+int i;
+
+for (i = 0; newStr[i] != '\0'; newStr++)
+{
+if (newStr[i] % 2 != 0)
+continue;
+
+_putchar(newStr[i]);
+}
+_putchar('\n');
+}
