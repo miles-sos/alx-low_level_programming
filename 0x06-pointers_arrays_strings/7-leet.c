74b1ce567858a2932ad0411b2351a3964cec11a9
miles-sos <frank.ebuzoeme@gmail.com>
2023-07-02 12:01:49 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * leet - a function that encodes a string into 1337
  * @str: string to be encoded
@@ -12,16 +11,15 @@ char *leetChars = "aAeEoOtTlL";
 char *charReplacement = "4433007711";
 
 int i; 
-unsigned int j;
+int j;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-for (j = 0; j < strlen(leetChars); j++)
+for (j = 0; j < 10; j++)
 {
 if (str[i] == leetChars[j])
 {
 str[i] = charReplacement[j];
-break;
 }
 }
 }
