832f51162bbecddfb0429301f472e589d2be86d1
miles-sos <frank.ebuzoeme@gmail.com>
2023-07-02 04:44:08 -0700

    Task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * cap_string - a function that capitalizes all words of a string.
+ * @str: the string to be capitalized
+ * Return: pointer to character
+ */
+char *cap_string(char *str)
+{
+	char *s = str;
+	int capitalizeNext = 1;
+
+	/* Loop through the string of chars */
+	while (*str != '\0')
+	{
+		if (capitalizeNext && isalpha(*str))
+		{
+			*str = toupper(*str);
+			capitalizeNext = 0;
+		}
+		else if (!isalpha(*str))
+		{
+			capitalizeNext = 1;
+		}
+		str++;
+	}
+	return (s);
+}
