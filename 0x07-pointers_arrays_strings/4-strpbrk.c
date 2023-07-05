75343b06ae7694250c98f696d24dcabd9112da4f
miles-sos <frank.ebuzoeme@gmail.com>
2023-07-04 19:55:43 -0700

    Task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,22 +3,21 @@
  * _strpbrk - a function that searches a
  * string for any of a set of bytes.
  * @s: string to be searched
- * @accept: string to locate 
+ * @accept: string to locate
  * Return: Always 0 (Success)
  */
 char *_strpbrk(char *s, char *accept)
 {
-                int a;
+int a;
 
-                while (*s)
-                {
-                        for (a = 0; accept[a]; a++)
-                        {
-                        if (*s == accept[a])
-                        return (s);
-                        }
-                s++;
-                }
-
-        return ('\0');
+while (*s)
+{
+for (a = 0; accept[a]; a++)
+{
+if (*s == accept[a])
+return (s);
+}
+s++;
+}
+return ('\0');
 }
