e484eaebfbc1a382c95fa2b16b90c10f5a4231d0
miles-sos <frank.ebuzoeme@gmail.com>
2023-08-02 19:46:05 -0700

    Updated Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,14 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t c = 0;
+	const listint_t *tmp;
 
-	while (h != NULL)
+	tmp = h;
+
+	while (tmp != NULL)
 	{
-		printf("%d \n", h->n);
-		h = h->next;
+		printf("%d \n", tmp->n);
+		tmp = tmp->next;
 		c++;
 	}
 	return (c);
