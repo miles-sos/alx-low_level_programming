9ea6c49d499b251eecee4ac897710a3292ab59cc
miles-sos <frank.ebuzoeme@gmail.com>
2023-08-11 17:06:00 -0700

    Added Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,17 +13,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* open the file, append the content */
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-
 	/* get length of text_content */
 	if (text_content == NULL)
 	{
 		close(fd);
 		return (1);
 	}
+
+	/* open the file, append the content */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
 
 	/* append to the file */
 	bytes_w = write(fd, text_content, strlen(text_content));
